d20c0fdb9e6cc7e487105b45e06cfae050da2353
ssamuel56 <samuelkirkendoll88@gmail.com>
2018-10-15 11:22:16 -0400

    0 doesnt return a empty string

--- numerizer.rb
+++ numerizer.rb
@@ -2,16 +2,14 @@ def numerals
   num_key = {
     1000 => "M", 900 => "CM", 500 => "D", 400 => "CD", 100 => "C",
     90 => "XC", 50 => "L", 40 => "XL", 10 => "X", 9 => "IX", 5 => "V",
-    3 => "IV", 1 => "I", 0 => ""
+    3 => "IV", 1 => "I"
   }
 end
 
 def romanize(n)
-  numerals_reversed = numerals.invert
   roman = ""
-
-  numerals.each do |num, value|
-    roman += num*(n / value)
+  numerals.each do |value, letter|
+    roman << letter*(n / value)
     n = n % value
   end
   return roman
